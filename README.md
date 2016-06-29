6ced8275a30ec41dd02041b3c3d7855463b2a7f5
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2016-06-29 17:02:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+App that disables the "Stay logged in" button on the owncloud login page
+
+# Install
+1. Clone this app to the <owncloud>/app/disablestayloggedin directory
+2. Enable the app
---
+++ appinfo/application.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * OwnCloud - Disable stay logged in
+ *
+ * PHP Version 5-7
+ *
+ * @category  Owncloud
+ * @package   Dummy
+ * @author    EUDAT <b2drop-devel@postit.csc.fi>
+ * @copyright 2016 EUDAT
+ * @license   AGPL3 https://github.com/EUDAT-B2DROP//b2drop_disable_stayloggedin/blob/master/LICENSE
+ * @link      https://github.com/EUDAT-B2DROP//b2drop_disable_stayloggedin.git
+ */
